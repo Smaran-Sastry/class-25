6724d55fb6c4aad5253dc5ce5df2e1433cca44dc
Smaran-Sastry <73423744+Smaran-Sastry@users.noreply.github.com>
2020-11-09 15:18:21 +0530

    Add files via upload

---
+++ Box.js
@@ -0,0 +1,10 @@
+class Box extends BaseClass {
+  constructor(x, y, width, height) {
+    super(x,y,width,height);
+    this.image = loadImage("sprites/wood1.png");
+    //Matter.Body.setAngle(this.body);
+}
+  display() {
+   super.display();
+  }
+};
